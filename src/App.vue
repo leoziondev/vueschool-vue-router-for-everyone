2053a541ed85ef2ffbc197e119160ce024356d76
LeoZion <leozionsuporte@gmail.com>
2022-08-09 19:53:40 -0300

    creating routes and views

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,37 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <h1>Vue<span>Travel</span></h1>
+      <nav>
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+        <router-link to="/brazil">Brazil</router-link>
+        <router-link to="/hawaii">Hawaii</router-link>
+        <router-link to="/jamaica">Jamaica</router-link>
+        <router-link to="/panama">Panama</router-link>
+      </nav>
     </div>
     <router-view />
   </div>
 </template>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  background: #f8fafc;
+}
+a {
+  text-decoration: none;
+  color: #2c3e50;
+}
+a:hover,
+#nav a:hover {
+  color: #42b983;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -18,12 +41,26 @@
 }
 
 #nav {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
   padding: 30px;
+  background: white;
+}
+
+#nav h1 {
+  font-size: 1.6rem;
+}
+
+#nav h1 span {
+  color: #42b983;
 }
 
 #nav a {
   font-weight: bold;
   color: #2c3e50;
+  padding: 0 10px;
 }
 
 #nav a.router-link-exact-active {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,67 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>All destinations</h1>
+    <div class="destinations">
+      <div v-for="destination in destinations" :key="destination.name">
+        <router-link :to="destination.slug">
+          <h2>{{ destination.name }}</h2>
+        </router-link>
+        <figure>
+          <router-link :to="destination.slug">
+            <img
+              :src="require(`@/assets/${destination.image}`)"
+              :alt="destination.name"
+            />
+          </router-link>
+        </figure>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import store from "@/store.js";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      destinations: store.destinations,
+    };
   },
 };
 </script>
+<style scoped>
+.home {
+  max-width: 1140px;
+  margin: 50px auto;
+}
+
+.destinations {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 20px;
+  margin-top: 30px;
+}
+
+.destinations img {
+  /* max-width: 200px; */
+  width: 100%;
+  object-fit: cover;
+}
+
+@media (max-width: 1024px) {
+  .destinations {
+    grid-template-columns: repeat(2, 1fr);
+    padding: 0 20px;
+  }
+}
+
+@media (max-width: 560px) {
+  .destinations {
+    grid-template-columns: repeat(1, 1fr);
+  }
+}
+</style>
