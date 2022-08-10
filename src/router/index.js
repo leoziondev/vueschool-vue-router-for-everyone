9494d3e5d2d4fc24b38345f204696e0c56f5d384
LeoZion <leozionsuporte@gmail.com>
2022-08-10 02:42:32 -0300

    pass router params as props to components

--- src/router/index.js
+++ src/router/index.js
@@ -9,16 +9,19 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+    props: true,
   },
   {
-    path: "/details/:id",
+    path: "/details/:slug",
     name: "DestinationDetails",
+    props: true,
     component: () =>
       import(/* webpackChunckName: "details" */ "../views/DestinationDetails"),
   },
 ];
 
 const router = new VueRouter({
+  mode: "history",
   routes,
 });
 
